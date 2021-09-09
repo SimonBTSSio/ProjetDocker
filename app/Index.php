f5ce5bded091024ab4da36953ac929248e54541a
SimonBTSSio <simon.farnault.sio@gmail.com>
2021-09-09 17:10:19 +0200

    pjDocker, MC6

--- app/Index.php
+++ app/Index.php
@@ -1,37 +1,32 @@
 <html>
 	<head>
+		<link rel="stylesheet" href="css/style.css" />
 		<title>Projet docker</title>
 	</head>
 	<body>
-		<h1>Les utilisateurs:</h1>
 		<?php
-			try {
+			try{
 				$dbh = new PDO('mysql:host=db;dbname=siteWeb', 'root', 'root' );
-				
-				}
-			
-				catch(Exception $e)
-				{
-					// En cas d'erreur, on affiche un message et on arrête tout
-						die('Erreur : '.$e->getMessage());
-				}
-			
+			}
+			catch(Exception $e){
+				die('Erreur : '.$e->getMessage());
+			}
 			$users = $dbh->query('SELECT * FROM user');
-
 			$data = $users->fetch();
-
-
-			echo "<table border='1'>
-			<tr>
-			<th>id</th>
-			<th>pseudo</th>
-			<th>nom</th>
-			<th>prenom</th>
-			</tr>";
-
-
-			while ($data = $users->fetch())
-			{
+		?>
+		<table>
+			<caption>Liste des utilisateurs</caption>
+  			<thead>
+    				<tr>
+	      				<th>Id</th>
+      					<th>Pseudo</th>
+					<th>Nom</th>
+     					<th>Prenom</th>
+   			 	</tr>
+  			</thead>
+			<tbody>
+		<?php
+			while ($data = $users->fetch()){
 				echo "<tr>";
 				echo "<td>" . $data['id'] . "</td>";
 				echo "<td>" . $data['pseudo'] . "</td>";
@@ -39,11 +34,12 @@
 				echo "<td>" . $data['prenom'] . "</td>";
 				echo "</tr>";
 			}
-			echo "</table>";
-
 			$users->closeCursor();
-
-
 		?>
+		</tbody>
+		</table>
+		<div class="wrap">
+  			<a href	="form.php" class="button">Inscription</a>
+		</div>
 	</body>
 </html>
