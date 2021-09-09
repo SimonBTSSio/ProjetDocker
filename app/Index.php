f8a097332a77855cac3eeeef2d7a7e317997fec8
Ayman-BEDDA <bedda.ayman.sio@gmail.com>
2021-09-09 15:17:19 +0200

    pjDocker , MC4

--- app/Index.php
+++ app/Index.php
@@ -3,9 +3,47 @@
 		<title>Projet docker</title>
 	</head>
 	<body>
-		<h1>ceci est un test</h1>
+		<h1>Les utilisateurs:</h1>
 		<?php
-			echo "test";
+			try {
+				$dbh = new PDO('mysql:host=db;dbname=siteWeb', 'root', 'root' );
+				
+				}
+			
+				catch(Exception $e)
+				{
+					// En cas d'erreur, on affiche un message et on arrête tout
+						die('Erreur : '.$e->getMessage());
+				}
+			
+			$users = $dbh->query('SELECT * FROM user');
+
+			$data = $users->fetch();
+
+
+			echo "<table border='1'>
+			<tr>
+			<th>id</th>
+			<th>pseudo</th>
+			<th>nom</th>
+			<th>prenom</th>
+			</tr>";
+
+
+			while ($data = $users->fetch())
+			{
+				echo "<tr>";
+				echo "<td>" . $data['id'] . "</td>";
+				echo "<td>" . $data['pseudo'] . "</td>";
+				echo "<td>" . $data['nom'] . "</td>";
+				echo "<td>" . $data['prenom'] . "</td>";
+				echo "</tr>";
+			}
+			echo "</table>";
+
+			$users->closeCursor();
+
+
 		?>
 	</body>
 </html>
--- app/form.php
+++ app/form.php
@@ -4,5 +4,20 @@
 	</head>
 	<body>
 		<h1>Formulaire</h1>
+		<form method="post" action="index.php">
+ 
+		<p>
+			
+		
+			<input type="text" name="pseudo" />	
+		
+			<input type="password" name="mdp" />
+
+			<input type="submit" value="Valider" />
+
+		</p>
+
+ 
+		</form>
 	</body>
 </html>
